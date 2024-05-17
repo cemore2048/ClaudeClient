040df93f3195323f16074e53b32c196c829da87a
Rafa <rmoreno.cesar@gmail.com>
2024-05-17 15:34:35 -0500

    update git ignore

--- app/src/main/java/com/cemore/claudeclient/ChatFragment.kt
+++ app/src/main/java/com/cemore/claudeclient/ChatFragment.kt
@@ -1,11 +1,16 @@
+import android.graphics.drawable.PaintDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -14,11 +19,15 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
+import com.cemore.claudeclient.R
 import com.cemore.claudeclient.TextFieldValueSaver
 import com.cemore.claudeclient.ui.theme.ClaudeClientTheme
 import com.cemore.claudeclient.ui.theme.Composer
@@ -43,7 +52,23 @@ class ChatFragment: Fragment() {
                                 TextFieldValue()
                             ) }
 
-                        Box(modifier = Modifier.align(Alignment.BottomCenter),) {
+                        
+                        Row(modifier = Modifier.align(Alignment.BottomCenter),) {
+                            Image(
+                                modifier = Modifier.size(24.dp),
+                                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                                contentDescription = null,
+                            )
+                            Image(
+                                modifier = Modifier.size(8.dp),
+                                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                                contentDescription = null,
+                            )
+                            Image(
+                                modifier = Modifier.size(8.dp),
+                                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                                contentDescription = null,
+                            )
                             Composer(
                                 textFieldValue = textFieldValue.value,
                                 onTextChanged = { entry ->
